44a2b198fafb3756400426c06925b1a796b670e8
thibaudszy <thivaudszy@gmail.com>
2021-05-11 21:25:57 +0200

    css change

--- pages/index.vue
+++ pages/index.vue
@@ -44,12 +44,7 @@ export default {
 }
 */
 .container {
-  /* margin: 0 auto;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center; */
 }
 
 .title {
