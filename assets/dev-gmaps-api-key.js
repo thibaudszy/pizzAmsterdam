db846c85ebe11d66c3712cb1833db3992be7ab43
thibaudszy <thivaudszy@gmail.com>
2021-10-17 14:46:34 +0200

    Only load DEV_API_KEY in dev environment

---
+++ assets/dev-gmaps-api-key.js
@@ -0,0 +1,2 @@
+const DEV_API_KEY = null;
+export default DEV_API_KEY;
--- plugins/gmaps.js
+++ plugins/gmaps.js
@@ -2,9 +2,14 @@ import Vue from 'vue';
 import * as VueGoogleMaps from 'vue2-google-maps';
 import DEV_API_KEY from '~/assets/dev-gmaps-api-key';
 
+const environment = process.env.NODE_ENV || 'development';
+let apikey = '';
+if (environment === 'development') {
+  apikey = DEV_API_KEY;
+}
 Vue.use(VueGoogleMaps, {
   load: {
-    key: DEV_API_KEY || 'AIzaSyArszZeFgAR134GcoUJ1mka6ADh9PfYbnY',
+    key: apikey || 'AIzaSyArszZeFgAR134GcoUJ1mka6ADh9PfYbnY',
     libraries: 'places',
   },
 });
